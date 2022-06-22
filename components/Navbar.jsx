5e91a4828f70180fd3f759f87396f9337fe91dad
mARUwTC <chriswong0621@gmail.com>
2022-06-22 20:08:08 +0800

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
 import styles from '../styles/Navbar.module.css';
+import Link from 'next/link'
 
 export default function Navbar() {
     return(
         <div className={styles.Navbar}>
-            <a class="active" href="/">Home</a>
+            <Link href="/"><a class="active">Home</a></Link>
         </div>
     )
 }

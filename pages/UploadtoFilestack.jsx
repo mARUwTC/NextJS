bc446568cd306cbc9e9eafda99b47b0d6b4ce3ec
mARUwTC <chriswong0621@gmail.com>
2022-06-23 15:37:46 +0800

    update

--- pages/UploadtoFilestack.jsx
+++ pages/UploadtoFilestack.jsx
@@ -2,17 +2,26 @@ import React, { useState } from 'react';
 import styles from '../styles/UploadtoFilestack.module.css'
 import * as filestack from 'filestack-js';
 
-const client = filestack.init("ApdLzyLEKReKPidpb0rR3z");
-
-const handleClick = () => {
-    client.picker().open();
-}
-
 function UploadtoFilestack() {
+
+    const client = filestack.init("ApdLzyLEKReKPidpb0rR3z");
+
+    const options = {
+        onFileUploadFinished(file) {
+            console.log(file.url);
+        }
+    }
+
+    const handleClick = () => {
+        client.picker(options).open();
+    }
+
     return (
+        <>
         <div className={styles.UploadtoFilestack}>
             <button onClick={handleClick}>Upload</button>
         </div>
+        </>
     )
 }
 
